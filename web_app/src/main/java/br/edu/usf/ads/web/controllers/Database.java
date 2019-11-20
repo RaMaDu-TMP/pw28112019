5d187f4236000b7795a059821572461ae57b2956
Eduwardo Horibe <eduhoribe@gmail.com>
2019-11-20 14:49:09 -0300

    database class

--- web_app/src/main/java/br/edu/usf/ads/web/controllers/Database.java
+++ web_app/src/main/java/br/edu/usf/ads/web/controllers/Database.java
@@ -5,18 +5,32 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
-    private static final String URL = "jdbc:mysql://localhost/aulaweb";
+    private static final Database instance;
+    static {
+        instance = new Database();
+    }
 
-    public Connection connection() {
+    private static final String host = "localhost";
+    private static final String db = "pw28112019";
+    private static final String user = "root";
+    private static final String pass = "";
+
+   private static Connection conn;
+
+    private Database () {
+        String url = "jdbc:mysql://" + host +"/" + db + "";
+
         try {
             Class.forName("com.mysql.jdbc.Driver");
-
-            return DriverManager.getConnection(URL, "root", "");
+            conn = DriverManager.getConnection(url, user, pass);
 
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
             System.out.println(e.getMessage());
         }
-        return null;
+    }
+
+    public static Connection connection() {
+        return conn;
     }
 }
