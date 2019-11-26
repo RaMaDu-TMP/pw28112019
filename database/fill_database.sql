d38174b7a117fd0a990c47d96cff15dc955bbe23
Eduwardo Horibe <eduhoribe@gmail.com>
2019-11-25 23:07:39 -0300

    fix fill database script

--- database/fill_database.sql
+++ database/fill_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 23/11/2019 às 19:15
+-- Tempo de geração: 26/11/2019 às 03:03
 -- Versão do servidor: 10.4.6-MariaDB
 -- Versão do PHP: 7.3.9
 
@@ -80,11 +80,20 @@ INSERT INTO `cursos` (`id`, `nome`) VALUES
 CREATE TABLE `disciplinas` (
   `id` int(11) NOT NULL,
   `nome` text NOT NULL,
-  `turno` ENUM('MATUTINO', 'VESPERTINO', 'NOTURNO') NOT NULL,
-  `dia_semana` ENUM('SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB', 'DOM') NOT NULL,
+  `turno` text NOT NULL,
+  `dia_semana` text NOT NULL,
   `curso_id` int(11) NOT NULL,
   `professor_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Despejando dados para a tabela `disciplinas`
+--
+
+INSERT INTO `disciplinas` (`id`, `nome`, `turno`, `dia_semana`, `curso_id`, `professor_id`) VALUES
+(1, 'Química dos materiais', 'MATUTINO', 'SAB', 1, 1),
+(2, 'Programação Orientada a Objetos', 'VESPERTINO', 'SEX', 5, 4),
+(3, 'Programação Web', 'NOTURNO', 'TER', 5, 4);
 
 -- --------------------------------------------------------
 
@@ -236,7 +245,7 @@ ALTER TABLE `grupos`
 -- AUTO_INCREMENT de tabela `professores`
 --
 ALTER TABLE `professores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `projetos`
